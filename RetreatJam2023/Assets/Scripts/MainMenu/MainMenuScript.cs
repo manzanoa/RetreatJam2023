13e05cbc0ef2c4053175e1c822cb97a8483cf358
manzanoa <manzanoa@vcu.edu>
2023-11-12 17:04:36 -0500

    Settings, Credits

--- RetreatJam2023/Assets/Scripts/MainMenu/MainMenuScript.cs
+++ RetreatJam2023/Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    [SerializeField] GameObject mainMenu, settings, tutorials, credits;
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
@@ -12,11 +13,28 @@ public class MainMenuScript : MonoBehaviour
 
     public void Settings()
     {
-
+        mainMenu.SetActive(false);
+        settings.SetActive(true);
     }
 
     public void Tutorial()
     {
+        mainMenu.SetActive(false);
+        tutorials.SetActive(true);
+    }
 
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void Back()
+    {
+        
+        settings.SetActive(false);
+        tutorials.SetActive(false);
+        credits.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
